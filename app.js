11bac937e42f31495e9bd50909cfe239d8203d85
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-05-21 11:03:21 -0400

    inquirer successfully installed

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const fs =require('fs');
-const http = require('http')
-var inquirer = require('inquirer')
-const generatePage = require('./src/page-template.js')
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+const inquirer = require("inquirer");
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [user,github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-console.log(user);
-console.log(github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-
-
-
-
-fs.writeFile('./index.html',generatePage(user,github), ohFuck=>{
-if(ohFuck) throw ohFuck;
-console.log('Filesaved!');
-});
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
--- src/page-template.js
+++ src/page-template.js
@@ -1,26 +1,19 @@
-const fs = require("fs");
-const profileDataArgs = process.argv.slice(2);
-const [user, github] = profileDataArgs;
-
-const generatePage = (userName, githubName) => {
+module.exports = (name, github) => {
   return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
+  <!DOCTYPE html>
+  <html lang="en">
+  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
   </head>
-
-
-
+  
   <body>
-    <h1>${user}</h1>
+    <h1>${name}</h1>
     <h2><a href="https://github.com/${github}">Github</a></h2>
   </body>
   </html>
   `;
 };
-
-module.exports = generatePage;
